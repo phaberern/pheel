8ce1726c15b5dc05a23de1121c47d0917e883caf
Phil <phillip.haberern5@gmail.com>
2017-08-24 19:27:39 -0400

    static fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('public'))
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // Starts the server to begin listening
